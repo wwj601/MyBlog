cd127ddcba13513d5cd677cd8c76c3234fe392b0
wanwenjie <15890537525@163.com>
2018-09-13 17:14:46 +0800

    链接服务器数据库

--- Schema/config.js
+++ Schema/config.js
@@ -1,7 +1,7 @@
 // 连接数据库 导出Schema
 var mongoose = require('mongoose')
 
-var db = mongoose.createConnection('mongodb://localhost:27017/blogproject',{useNewUrlParser:true})
+var db = mongoose.createConnection('mongodb://123.207.211.211:27017/blogproject',{useNewUrlParser:true})
 
 // 用原生es6 Promise 代替mongoose 自实现的 Promise
 mongoose.Promise = global.Promise
